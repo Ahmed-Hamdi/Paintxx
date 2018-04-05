4d363a77b183ff18337490672bfa7a476536deb4
dizzyeasy <zeyad.a.sharaf@gmail.com>
2018-04-05 15:33:39 +0200

    Trial By Me Zeyad

---
+++ Pain/src/PaintProject/Democlass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PaintProject;
+
+/**
+ *
+ * @author hp_pc
+ */
+ 
+public class Democlass {
+public static void main (String[] args){
+    System.out.println("This is me zeyad trying git");
+    Parentclass ch=new Parentclass("Zeyad",19);
+    ch.speakfrench();
+    
+}
+}
---
+++ Pain/src/PaintProject/Parentclass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PaintProject;
+
+/**
+ *
+ * @author dell-pc
+ */
+public class Parentclass {
+    String name;
+    int age;
+    Parentclass(String name,int age)
+    {
+        this.name=name;
+        this.age=age;
+    }
+    public void speakfrench()
+    {
+        System.out.println("Je parle anglais");
+    }
+}
