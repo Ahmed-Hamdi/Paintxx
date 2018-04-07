008ec4fedd0ec96087e19ef47484819747824300
hp\hp_pc <ahmedabdelhamid106@gmail.com>
2018-04-07 11:25:24 +0200

    s

--- Pain/src/PaintProject/Democlass.java
+++ Pain/src/PaintProject/Democlass.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package PaintProject;
 
-/**
- *
- * @author hp_pc
- */
- 
+
 public class Democlass {
 public static void main (String[] args){
     System.out.println("This is me zeyad trying git");
