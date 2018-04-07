7c7ddb76549654878b97eadea135f2707064697f
hp\hp_pc <ahmedabdelhamid106@gmail.com>
2018-04-07 13:16:33 +0200

    z

--- Paint/src/paint/FXMLDocumentController.java
+++ Paint/src/paint/FXMLDocumentController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package paint;
 
 import java.net.URL;
@@ -12,10 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-/**
- *
- * @author hp_pc
- */
+
 public class FXMLDocumentController implements Initializable {
 
 @FXML
@@ -23,7 +16,6 @@ private Label label;
 
 @FXML
 private void handleButtonAction(ActionEvent event) {
-System.out.println("You clicked me!");
 label.setText("Hello World!");
 }
 
